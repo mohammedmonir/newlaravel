92da02471aa8f805076c34bfdb1811b3afaa9ca9
Mohammed Monir <abu1997tariq@hotmail.com>
2019-07-29 10:01:20 -0700

    before reverse

--- resources/views/dashboard/categories/create.blade.php
+++ resources/views/dashboard/categories/create.blade.php
@@ -1,55 +1,47 @@
 @extends('dashboard.app')
 @section('content')
-    <!-- محتوى الصفحة -->
-
-  
-    <div class="content-wrapper">
-
-        <section class="content-header">
-
-               <h1> @lang('site.add')</h1>
-                <ol class="breadcrumb">
-                <li><a href="{{url('dashboard/index')}}"><i class="fa fa-dashboard"></i>@lang('site.dashboard')</a></li>
-                <li><a href="{{url('dashboard/categories')}}">@lang('site.categories')</a></li>
-                <li><a href="{{url('dashboard/categories/create')}}">@lang('site.add')</a></li>
-             
-            </ol>
-        </section>
-
-        <section class="content">
-
-            <div class="box-body">
-                @include('dashboard.layouts.errors')
-                <form action="{{ route('categories.store') }}" method="post">
-                    {{ csrf_field() }}
-                    {{ method_field('post') }}
+<!-- محتوى الصفحة -->
 
 
-                    @foreach(config('translatable.locales') as $locale)
+<div class="content-wrapper">
 
-                        <div class='form-group'>
-                            <label>@lang('site.'.$locale.'.name')</label>
-                            <input type="text" name="{{ $locale }}[name]" class='form-control' value='{{old($locale.".name")}}'>
-                        </div>
-                        
-                    @endforeach
+    <section class="content-header">
 
-                    
+        <h1> @lang('site.add')</h1>
+        <ol class="breadcrumb">
+            <li><a href="{{url('dashboard/index')}}"><i class="fa fa-dashboard"></i>@lang('site.dashboard')</a></li>
+            <li><a href="{{url('dashboard/categories')}}">@lang('site.categories')</a></li>
+            <li><a href="{{url('dashboard/categories/create')}}">@lang('site.add')</a></li>
+
+        </ol>
+    </section>
+
+    <section class="content">
+
+        <div class="box-body">
+            @include('dashboard.layouts.errors')
+            <form action="{{ route('categories.store') }}" method="post">
+                {{ csrf_field() }}
+                {{ method_field('post') }}
 
 
-                    
-                
+                @foreach(config('translatable.locales') as $locale)
 
-                    
-                   
+                <div class='form-group'>
+                    <label>@lang('site.'.$locale.'.name')</label>
+                    <input type="text" name="{{ $locale }}[name]" class='form-control' value='{{old($locale.".name")}}'>
+                </div>
 
-                    <div class='form-group'>
-                        <button type='submit' class='btn btn-primary'><i class='fa fa-plus'></i>@lang("site.add")</button>
-                    </div>
-                </form>
-            </div>
+                @endforeach
 
-        </section>
-    </div>
+
+                <div class='form-group'>
+                    <button type='submit' class='btn btn-primary'><i class='fa fa-plus'></i>@lang("site.add")</button>
+                </div>
+            </form>
+        </div>
+
+    </section>
+</div>
 
 @endsection
--- resources/views/dashboard/categories/edit.blade.php
+++ resources/views/dashboard/categories/edit.blade.php
@@ -1,47 +1,47 @@
 @extends('dashboard.app')
 @section('content')
-    <!-- محتوى الصفحة -->
+<!-- محتوى الصفحة -->
 
-  
-    <div class="content-wrapper">
 
-        <section class="content-header">
+<div class="content-wrapper">
 
-               <h1> @lang('site.edit')</h1>
-                <ol class="breadcrumb">
-                <li><a href="{{url('dashboard/index')}}"><i class="fa fa-dashboard"></i>@lang('site.dashboard')</a></li>
-                <li><a href="{{url('dashboard/categories')}}">@lang('site.categories')</a></li>
-                <li><a href="#">@lang('site.edit')</a></li>
-             
-            </ol>
-        </section>
+    <section class="content-header">
 
-        <section class="content">
+        <h1> @lang('site.edit')</h1>
+        <ol class="breadcrumb">
+            <li><a href="{{url('dashboard/index')}}"><i class="fa fa-dashboard"></i>@lang('site.dashboard')</a></li>
+            <li><a href="{{url('dashboard/categories')}}">@lang('site.categories')</a></li>
+            <li><a href="#">@lang('site.edit')</a></li>
 
-            <div class="box-body">
+        </ol>
 
-                @include('dashboard.layouts.errors')    
+    </section>
 
-             
-                <form action="{{ route('categories.update', $category->id) }}" method="POST" >
-                     {{ method_field('put') }}
-                    {{ csrf_field() }}
-                  
-                    
-                    <div class='form-group'>
-                        <label>@lang('site.name')</label>
-                        <input type="text" name="name" class='form-control' value='{{$category->name}}'>
-                    </div>
-                    
-               
+    <section class="content">
+        <div class="box-body">
 
-                    <div class='form-group'>
-                        <button type='submit' class='btn btn-primary'> <i class='fa fa-edit'></i>@lang("site.edit")</button>
-                    </div>
-                </form>
-            </div>
+            @include('dashboard.layouts.errors')
 
-        </section>
-    </div>
+
+            <form action="{{ route('categories.update', $category->id) }}" method="POST">
+                {{ method_field('put') }}
+                {{ csrf_field() }}
+
+
+                <div class='form-group'>
+                    <label>@lang('site.name')</label>
+                    <input type="text" name="name" class='form-control' value='{{$category->name}}'>
+                </div>
+
+
+
+                <div class='form-group'>
+                    <button type='submit' class='btn btn-primary'> <i class='fa fa-edit'></i>@lang("site.edit")</button>
+                </div>
+            </form>
+        </div>
+
+    </section>
+</div>
 
 @endsection
