96c86aa5c70b65176eecb424eaa0a14d6ec853c6
Mohammed Monir <abu1997tariq@hotmail.com>
2019-08-12 05:40:48 -0700

    'laravel-27'

--- public/dashboard/dist/js/order.js
+++ public/dashboard/dist/js/order.js
@@ -4,22 +4,40 @@ $(document).ready(function () {
     $('.add-product-btn').on('click', function (e) {
 
         e.preventDefault();
-      
-            var name =$(this).data('name');
-            var id =$(this).data('id');
-            var price =$(this).data('price');
-           
-            var html =
-                `<tr>
-                    <td>${name}</td>
-                    <td><input type="number" name="products[${id}][quantity]" data-price="${price}" class="form-control input-sm product-quantity" min="1" value="1"></td>
-                    <td class="product-price">${price}</td>               
-                    <td><button class="btn btn-danger btn-sm remove-product-btn" data-id="${id}"><span class="fa fa-trash"></span></button></td>
-                </tr>`;
+        var name = $(this).data('name');
+        var id = $(this).data('id');
+        var price = $(this).data('price')
 
-        $('.order-list').append(html);//عرض حقل ال html في كلاس اوردرليست
+        $(this).removeClass('btn-success').addClass('btn-default disabled');
+
+        var html =
+            `<tr>
+                <td>${name}</td>
+                <td><input type="number" name="products[${id}][quantity]" data-price="${price}" class="form-control input-sm product-quantity" min="1" value="1"></td>
+                <td class="product-price">${price}</td>               
+                <td><button class="btn btn-danger btn-sm remove-product-btn" data-id="${id}"><span class="fa fa-trash"></span></button></td>
+            </tr>`;
+
+        $('.order-list').append(html);//عرض حقل ال html في كلاس
     });
+
+        $('body').on('click', '.disabled', function(e) {
+
+            e.preventDefault();
+
+        });
+
+      
+        $('body').on('click', '.remove-product-btn', function(e) {
+
+            e.preventDefault();
+            var id = $(this).data('id');
+
+            $(this).closest('tr').remove();
+            $('#product-' + id).removeClass('btn-default disabled').addClass('btn-success');
+        });
+    
 
     
 
-});//end of calculate total
+});
